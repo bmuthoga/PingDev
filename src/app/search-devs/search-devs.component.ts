c994b45ec6f9ad6cf76348cf49573315d28a0eb1
Batian Muthoga <bmuthoga@gmail.com>
2017-09-01 14:28:11 +0300

    feat(search-devs): add 'Bootstrap 4' styling
    - add 'Bootstrap 4' css to 'styles.css'
    - edit 'search-devs html' template
      - add 'Location' input field
      - add 'Language' input field
      - add 'Search' button
    - edit 'search-devs component'
      - add 'place' and 'language' properties
      - add 'search' method
    - edit 'search-devs html' template
      - add 'click' event to 'Search' button

--- src/app/search-devs/search-devs.component.ts
+++ src/app/search-devs/search-devs.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-search-devs',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-devs.component.css']
 })
 export class SearchDevsComponent implements OnInit {
+  place: string;
+  language: string;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  search(place: string, language: string) {
+    this.place = place;
+    this.language = language;
+    console.log(this.place, this.language);
   }
 
 }
