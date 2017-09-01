73ce7af210d92546fb63763796bc1b6b4e561795
Batian Muthoga <bmuthoga@gmail.com>
2017-09-01 14:06:38 +0300

    feat(search-devs): add 'search-devs' component
    - generate 'search-devs' component
    - import and declare 'search-devs' component in 'app module'
    - replace default placeholder with 'app-search-devs' directive in 'app component html' template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchDevsComponent } from './search-devs/search-devs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchDevsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/search-devs/search-devs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-devs',
+  templateUrl: './search-devs.component.html',
+  styleUrls: ['./search-devs.component.css']
+})
+export class SearchDevsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
